aa787b6a1dae052372ff472957c20f8459fd574b
Kaique_Tavares\HomePC <kaique-reino@hotmail.com.br>
2018-02-25 22:43:42 -0300

    Adição da Splash Screen

---
+++ app/src/main/java/com/example/logonrm/imc/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.logonrm.imc
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    val tempoDoSplash = 2500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //Chamando minha função
+        carregar()
+    }
+
+    fun carregar(){
+
+        Handler().postDelayed({
+            // Alt Enter+ mouse na classe que ficar vermelha para fazer o import do package
+            startActivity(Intent(this,FormActivity::class.java))
+            // Matando minha tela de splash
+            finish()
+        },tempoDoSplash)
+    }
+}
